752b54127c297538caa29c0d6c956dc2e5a72b65
Brian Vega <BrianVega01@outlook.es>
2024-10-30 13:00:26 -0600

    Request header Versioning

--- src/main/java/com/gd/bvega/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/com/gd/bvega/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -22,8 +22,19 @@ public class VersioningPersonController {
         return new PersonV1("Bob Charlie");
     }
 
-    @GetMapping(path = "person", params = "version=2")
+    @GetMapping(path = "/person", params = "version=2")
     public PersonV2 getSecondVersionOfPersonRequestParameter() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
 }
