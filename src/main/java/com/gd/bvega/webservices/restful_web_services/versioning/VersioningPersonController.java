8e3490fd5501db6857bdc14e0d52903a48e2b614
Brian Vega <BrianVega01@outlook.es>
2024-10-30 12:55:47 -0600

    Request param Versioning

--- src/main/java/com/gd/bvega/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/com/gd/bvega/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -1,6 +1,7 @@
 package com.gd.bvega.webservices.restful_web_services.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,16 @@ public class VersioningPersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 }
