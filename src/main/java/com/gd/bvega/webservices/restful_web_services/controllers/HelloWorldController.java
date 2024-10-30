9d6e174d686a6d54915acb39afb16be53461cf5d
Brian Vega <BrianVega01@outlook.es>
2024-10-30 12:17:50 -0600

    Internationalization for REST API, enabling Accept-language header in request

--- src/main/java/com/gd/bvega/webservices/restful_web_services/controllers/HelloWorldController.java
+++ src/main/java/com/gd/bvega/webservices/restful_web_services/controllers/HelloWorldController.java
@@ -1,10 +1,20 @@
 package com.gd.bvega.webservices.restful_web_services.controllers;
 
 import com.gd.bvega.webservices.restful_web_services.beans.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
@@ -25,4 +35,10 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello " + name);
     }
 
+    @GetMapping(path = "/hello-world/internationalized")
+    public String HelloWorldWithInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+    }
 }
