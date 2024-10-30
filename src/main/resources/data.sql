62a59cca1a7e6ead8ad021d4fb93bf94bb17b6ed
Brian Vega <BrianVega01@outlook.es>
2024-10-30 17:23:09 -0600

    Setting up h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details (id, birth_date, name)
+values (10001, current_date(), 'Brian');
+
+insert into user_details (id, birth_date, name)
+values (10002, current_date(), 'Oscar');
+
+insert into user_details (id, birth_date, name)
+values (10003, current_date(), 'Osvaldo');
