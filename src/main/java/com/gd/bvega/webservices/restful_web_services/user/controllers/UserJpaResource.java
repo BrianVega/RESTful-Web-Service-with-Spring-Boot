6a115892dff6fe95a9042f7142e84a4070d0c085
Brian Vega <BrianVega01@outlook.es>
2024-10-31 11:04:14 -0600

    Implementing post repository

--- src/main/java/com/gd/bvega/webservices/restful_web_services/user/controllers/UserJpaResource.java
+++ src/main/java/com/gd/bvega/webservices/restful_web_services/user/controllers/UserJpaResource.java
@@ -3,6 +3,7 @@ package com.gd.bvega.webservices.restful_web_services.user.controllers;
 import com.gd.bvega.webservices.restful_web_services.user.entities.Post;
 import com.gd.bvega.webservices.restful_web_services.user.entities.User;
 import com.gd.bvega.webservices.restful_web_services.user.UserNotFoundException;
+import com.gd.bvega.webservices.restful_web_services.user.repositories.PostRepository;
 import com.gd.bvega.webservices.restful_web_services.user.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
@@ -19,9 +20,11 @@ import java.util.Optional;
 public class UserJpaResource {
 
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
-    public UserJpaResource(UserRepository userRepository) {
+    public UserJpaResource(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @GetMapping(path = "/jpa/users")
@@ -69,4 +72,38 @@ public class UserJpaResource {
                 .toUri();
         return ResponseEntity.created(location).body(user);
     }
+
+    @PostMapping(path = "/jpa/users/{userId}/posts")
+    public ResponseEntity<Post> createPostForUser(@PathVariable Integer userId, @RequestBody Post post) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if(user.isEmpty())
+            throw new UserNotFoundException("User with id " + userId + " not found");
+
+        post.setUser(user.get());
+
+        Post savedPost = postRepository.save(post);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(post.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(savedPost);
+    }
+
+    @GetMapping(path = "/jpa/users/{userId}/posts/{postId}")
+    public ResponseEntity<Post> getPost(@PathVariable Integer userId, @PathVariable Integer postId) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if(user.isEmpty())
+            throw new UserNotFoundException("User with id " + userId + " not found");
+
+        Optional<Post> post = postRepository.findById(postId);
+
+        if(post.isEmpty())
+            throw new UserNotFoundException("Post with id " + postId + " not found");
+
+        return ResponseEntity.ok(post.get());
+    }
 }
+
--- src/main/java/com/gd/bvega/webservices/restful_web_services/user/entities/Post.java
+++ src/main/java/com/gd/bvega/webservices/restful_web_services/user/entities/Post.java
@@ -2,6 +2,7 @@ package com.gd.bvega.webservices.restful_web_services.user.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Post {
@@ -10,6 +11,7 @@ public class Post {
     @GeneratedValue
     private Integer id;
 
+    @Size(min = 10)
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
